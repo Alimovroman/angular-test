80eab9492a09fa595ca6b48089ae3e7081afc710
Roman <Alimov.ramon@gmail.com>
2024-03-11 17:14:59 +0300

    add property binding, event binding, two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Test1Component } from './test1/test1.component'
+import { Test2Component } from './test2/test2.component'
+import { FormsModule } from '@angular/forms'
+
+interface IUser {
+  name: string
+  age: number
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Test1Component, Test2Component],
+  imports: [RouterOutlet, Test1Component, Test2Component, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  appTitle = 'Instagram'
+  appText = ''
+  user: IUser = {
+    name: 'Roman',
+    age: 33,
+  }
+  isDisableButton = true
+
+  onChangeTitle() {
+    this.appTitle = this.appText
+  }
+  onChangeText(event: Event) {
+    this.appText = (event.currentTarget as HTMLInputElement).value
+    if (this.appText === '') {
+      this.isDisableButton = true
+    } else {
+      this.isDisableButton = false
+    }
+  }
+}
