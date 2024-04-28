a1b6b3aa8f4b0006857325a0ab6a54a3b3ed0dae
Roman <Alimov.ramon@gmail.com>
2024-04-28 16:29:27 +0300

    add lazyloading

--- src/app/shared/app.routes.ts
+++ src/app/shared/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router'
-import { Test2Component } from '../home/components/test2/test2.component'
+// import { Test2Component } from '../home/components/test2/test2.component'
 import { LoginComponent } from '../auth/components/login/login.component'
 import { Todos } from '../todos/components/todos/todos.component'
 import { FruitsComponent } from '../fruits/components/fruits/fruits.component'
@@ -9,8 +9,14 @@ import { ProfileComponent } from '../profile/components/profile/profile.componen
 import { authGuard } from '../guard/auth.guard'
 
 export const routes: Routes = [
-  { path: '', component: Test2Component },
-  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    loadChildren: () => import('../home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule),
+  },
   { path: 'todos', component: FruitsComponent, canActivate: [authGuard] },
   { path: 'fruits', component: Todos, canActivate: [authGuard] },
   { path: 'users', component: UsersComponent, canActivate: [authGuard] },
